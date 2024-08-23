3b560ab45dcca5e7e8dcac50cfc4cf1e533b4304
Yhethi <eloyjosebonillag@gmail.com>
2024-08-23 19:17:16 -0400

    v1.061

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ function App() {
       />
       {/* <ProductImage/> */}
       <div className="footer_text zIndexUp">
-        Hecho por Yhethi +584124706698 <p className="v_foot zIndexUp">v1.06</p>
+        Hecho por Yhethi +584124706698 <p className="v_foot zIndexUp">v1.061</p>
       </div>
     </div>
   );
